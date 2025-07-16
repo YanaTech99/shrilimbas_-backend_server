8d4cfd2dd628ba48b8606631774ea6247c5b2a93
NIKHIL-RHOPE <yanatechdigital@gmail.com>
2025-07-16 19:26:29 +0530

    create update shop route

--- src/assets/controller/shop.controller.js
+++ src/assets/controller/shop.controller.js
@@ -5,6 +5,8 @@ import {
   deleteFromCloudinary,
 } from "../utils/cloudinary.util.js";
 
+import { sanitizeInput } from "../utils/validation.util.js";
+
 const updateShop = async (req, res) => {
   const { id: userId } = req.user;
   const [shop] = await pool.execute(
@@ -33,7 +35,6 @@ const updateShop = async (req, res) => {
     "status",
     "email",
     "contact_alternate_phone",
-    "address_id",
     "is_verified",
     "is_featured",
     "categories",
@@ -86,6 +87,80 @@ const updateShop = async (req, res) => {
     return res
       .status(500)
       .json({ success: false, error: "Internal Server Error" });
+  }
+};
+
+const updateAddress = async (req, res) => {
+  const { id: user_id } = req.user;
+  const [shop_id] = await pool.query(`SELECT id FROM shops WHERE user_id = ?`, [
+    user_id,
+  ]);
+
+  if (!shop_id || shop_id.length === 0) {
+    return res.status(404).json({
+      success: false,
+      error: "Shop not found",
+    });
+  }
+
+  const modifiedInput = sanitizeInput(req.body);
+
+  const { address_id } = modifiedInput;
+  const {
+    address_line1,
+    address_line2,
+    landmark,
+    city,
+    state,
+    postal_code,
+    country,
+    address_type,
+  } = modifiedInput.address;
+
+  const client = await pool.getConnection();
+
+  try {
+    client.beginTransaction();
+
+    const updateQuery = [];
+
+    if (address_line1) updateQuery.push(`address_line1 = '${address_line1}'`);
+    if (address_line2) updateQuery.push(`address_line2 = '${address_line2}'`);
+    if (landmark) updateQuery.push(`landmark = '${landmark}'`);
+    if (city) updateQuery.push(`city = '${city}'`);
+    if (state) updateQuery.push(`state = '${state}'`);
+    if (postal_code) updateQuery.push(`postal_code = '${postal_code}'`);
+    if (country) updateQuery.push(`country = '${country}'`);
+    if (address_type) updateQuery.push(`address_type = '${address_type}'`);
+
+    const [result] = await client.query(
+      `UPDATE addresses SET ${updateQuery.join(
+        ", "
+      )} WHERE id = ? AND shop_id = ?`,
+      [address_id, shop_id[0].id]
+    );
+
+    if (result.affectedRows === 0) {
+      await client.rollback();
+      return res.status(500).json({
+        success: false,
+        error: "Failed to update address.",
+      });
+    }
+
+    await client.commit();
+    return res.status(200).json({
+      success: true,
+      message: "Address updated successfully.",
+    });
+  } catch (error) {
+    await client.rollback();
+    return res.status(500).json({
+      success: false,
+      error: "Failed to update address. Inernal server error.",
+    });
+  } finally {
+    client.release();
   }
 };
 
@@ -715,6 +790,164 @@ const addProducts = async (req, res) => {
   }
 };
 
+const updateProduct = async (req, res) => {
+  const { id: user_id, user_type } = req.user;
+  const [shop_id] = await pool.execute(
+    "SELECT id FROM shops WHERE user_id = ?",
+    [user_id]
+  );
+
+  if (user_type !== "VENDOR") {
+    return res.status(403).json({
+      success: false,
+      message: "Forbidden: Only vendors can update their products.",
+    });
+  }
+
+  if (!shop_id || !shop_id[0]) {
+    return res.status(404).json({
+      success: false,
+      message: "Shop not found for this vendor.",
+    });
+  }
+
+  const modifiedInput = sanitizeInput(req.body);
+
+  const {
+    id: product_id,
+    product_name,
+    barcode,
+    short_description,
+    long_description,
+    specifications,
+    mrp,
+    selling_price,
+    cost_price,
+    tax_percentage,
+    stock_quantity,
+    min_stock_alert,
+    stock_unit,
+    is_in_stock,
+    warehouse_location,
+    tags,
+    attributes,
+    is_featured,
+    is_new_arrival,
+    is_best_seller,
+    product_type,
+    status,
+    meta_title,
+    meta_description,
+    custom_fields,
+  } = modifiedInput;
+
+  const productImage = req.file;
+  let uploadedImage = null;
+
+  const [product] = await pool.execute(
+    `SELECT * FROM products WHERE id = ? AND shop_id = ?`,
+    [product_id, shop_id[0].id]
+  );
+
+  if (!product || !product[0]) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found.",
+    });
+  }
+
+  const client = await pool.getConnection();
+
+  try {
+    await client.beginTransaction();
+
+    const updateQuery = [];
+    const values = [];
+
+    if (product_name) updateQuery.push(`product_name = ?`);
+    if (barcode) updateQuery.push(`barcode = ?`);
+    if (short_description) updateQuery.push(`short_description = ?`);
+    if (long_description) updateQuery.push(`long_description = ?`);
+    if (specifications) updateQuery.push(`specifications = ?`);
+    if (mrp) updateQuery.push(`mrp = ?`);
+    if (selling_price) updateQuery.push(`selling_price = ?`);
+    if (cost_price) updateQuery.push(`cost_price = ?`);
+    if (tax_percentage) updateQuery.push(`tax_percentage = ?`);
+    if (stock_quantity) updateQuery.push(`stock_quantity = ?`);
+    if (min_stock_alert) updateQuery.push(`min_stock_alert = ?`);
+    if (stock_unit) updateQuery.push(`stock_unit = ?`);
+    if (is_in_stock) updateQuery.push(`is_in_stock = ?`);
+    if (warehouse_location) updateQuery.push(`warehouse_location = ?`);
+    if (tags) updateQuery.push(`tags = ?`);
+    if (attributes) updateQuery.push(`attributes = ?`);
+    if (is_featured) updateQuery.push(`is_featured = ?`);
+    if (is_new_arrival) updateQuery.push(`is_new_arrival = ?`);
+    if (is_best_seller) updateQuery.push(`is_best_seller = ?`);
+    if (product_type) updateQuery.push(`product_type = ?`);
+    if (status) updateQuery.push(`status = ?`);
+    if (meta_title) updateQuery.push(`meta_title = ?`);
+    if (meta_description) updateQuery.push(`meta_description = ?`);
+    if (custom_fields) updateQuery.push(`custom_fields = ?`);
+
+    updateQuery.push(`updated_at = NOW()`);
+
+    updateQuery.forEach((_, index) =>
+      values.push(modifiedInput[updateQuery[index]])
+    );
+
+    const [result] = await pool.execute(
+      `UPDATE products SET ${updateQuery.join(
+        ", "
+      )} WHERE id = ? AND shop_id = ?`,
+      [...values, product_id, shop_id[0].id]
+    );
+
+    if (result.affectedRows === 0) {
+      await client.rollback();
+      return res.status(404).json({
+        success: false,
+        message: "Failed to update product.",
+      });
+    }
+
+    if (productImage || productImage.path) {
+      uploadedImage = await uploadImageToCloudinary(productImage.path);
+
+      if (uploadedImage) {
+        const [result] = await pool.execute(
+          `UPDATE products SET thumbnail = ? WHERE id = ? AND shop_id = ?`,
+          [uploadedImage.secure_url, product_id, shop_id[0].id]
+        );
+
+        if (result.affectedRows === 0) {
+          await client.rollback();
+          return res.status(404).json({
+            success: false,
+            message: "Failed to update product image.",
+          });
+        }
+      }
+    }
+
+    await client.commit();
+    return res.status(200).json({
+      success: true,
+      message: "Product updated successfully.",
+    });
+  } catch (error) {
+    await client.rollback();
+    await deleteFromCloudinary(uploadedImage?.public_id);
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Failed to update product.",
+    });
+  } finally {
+    fs.unlinkSync(productImage.path);
+    client.release();
+  }
+};
+
 const getPaginatedproducts = async (req, res) => {
   const { id: user_id, user_type } = req.user;
 
@@ -884,9 +1117,11 @@ const getPaginatedproducts = async (req, res) => {
 
 export {
   updateShop,
+  updateAddress,
   addCategory,
   addBrand,
   addProducts,
+  updateProduct,
   getPaginatedCategories,
   getPaginatedBrands,
   getPaginatedproducts,
--- src/assets/router/shop.route.js
+++ src/assets/router/shop.route.js
@@ -6,6 +6,8 @@ import {
   getPaginatedBrands,
   getPaginatedCategories,
   getPaginatedproducts,
+  updateAddress,
+  updateProduct,
   updateShop,
 } from "../controller/shop.controller.js";
 import { authenticateToken } from "../middleware/auth.middleware.js";
@@ -15,6 +17,8 @@ const router = Router();
 
 router.route("/updateShop").patch(authenticateToken, updateShop);
 
+router.route("/updateAddress").patch(authenticateToken, updateAddress);
+
 router
   .route("/addBrand")
   .post(authenticateToken, upload.single("brandImage"), addBrand);
@@ -23,7 +27,7 @@ router.route("/addProduct").post(
   authenticateToken,
   upload.fields([
     { name: "thumbnail", maxCount: 1 },
-    { name: "gallery_images", maxCount: 10 },
+    { name: "gallery_images", maxCount: 5 },
     { name: "variant_thumbnail_0", maxCount: 1 },
   ]),
   addProducts
@@ -36,5 +40,9 @@ router
 router.route("/getCategories").get(authenticateToken, getPaginatedCategories);
 router.route("/getBrands").get(authenticateToken, getPaginatedBrands);
 router.route("/getProducts").get(authenticateToken, getPaginatedproducts);
+
+router
+  .route("/updateProduct")
+  .patch(authenticateToken, upload.single("thumbnail"), updateProduct);
 
 export default router;
